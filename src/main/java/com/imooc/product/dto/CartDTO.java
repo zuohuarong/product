4a00faf5f1dfc692f8d1162fcb2b2accbf3bd47c
zuohuarong <296674556@qq.com>
2020-08-03 18:27:37 +0800

    调用

--- src/main/java/com/imooc/product/dto/CartDTO.java
+++ src/main/java/com/imooc/product/dto/CartDTO.java
@@ -15,4 +15,10 @@ public class CartDTO {
   private String productId;
 
   private Integer productQuantity;
+
+  public CartDTO(String productId, Integer productQuantity) {
+    this.productId = productId;
+    this.productQuantity = productQuantity;
+  }
+
 }
